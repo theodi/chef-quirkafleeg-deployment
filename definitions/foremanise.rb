c9bc8fa774c5a2578623221e5e836bf5f05971f1
James Smith <james@floppy.org.uk>
2014-05-19 07:05:55 +0100

    include path in foreman export, to handle foreman > 0.65.0

--- definitions/foremanise.rb
+++ definitions/foremanise.rb
@@ -1,4 +1,17 @@
 define :foremanise, :params => {} do
+
+  script 'Generate foreman path environment' do
+      interpreter 'bash'
+      cwd params[:cwd]
+      user params[:user]
+      code <<-EOF
+        RVMDIR=/home/#{user}/.rvm
+        GEMDIR=`$RVMDIR/bin/rvm gemdir`
+        RUBY=`basename $GEMDIR | cut -d '@' -f 1`
+        echo "PATH=$RVMDIR/gems/$RUBY@global/bin:$RVMDIR/rubies/$RUBY/bin/" > /tmp/path_env
+      EOF
+  end
+
   script 'Start Me Up' do
     interpreter 'bash'
     cwd params[:cwd]
@@ -10,6 +23,7 @@ define :foremanise, :params => {} do
           -u #{params[:user]} \
           -l /var/log/#{params[:user]}/#{params[:name]} \
           -p #{params[:port]} \
+          -e #{params[:cwd]}/.env,/tmp/path_env \
           upstart /etc/init
     EOF
   end
